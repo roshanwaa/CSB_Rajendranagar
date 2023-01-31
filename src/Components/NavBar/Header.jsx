12357cc4a42aa5789be7669d937a7c3f35eea5d9
Mr.Smoke <kumroshan120@gmail.com>
2023-02-01 00:36:43 +0530

    change Path

--- src/Components/NavBar/Header.jsx
+++ src/Components/NavBar/Header.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { BrowserRouter as HashRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as HashRouter, Route, Routes } from 'react-router-dom';
 
-import { Navbar } from "./Navbar";
-import { Home } from "../Content/Home";
-import { MenuComponent } from "../Content/MenuComponent";
-import { About } from "../Content/About";
-import { Contact } from "../Content/Contact";
-import "./Header.css";
+import { About } from '../Content/About';
+import { Contact } from '../Content/Contact';
+import { Home } from '../Content/Home';
+import { MenuComponent } from '../Content/MenuComponent';
+import './Header.css';
+import { Navbar } from './Navbar';
 
 export const Header = () => {
   return (
@@ -15,7 +15,7 @@ export const Header = () => {
         <div className="container">
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/CSB_Rajendranagar" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/menu" element={<MenuComponent />} />
             <Route path="/about" element={<About />} />
