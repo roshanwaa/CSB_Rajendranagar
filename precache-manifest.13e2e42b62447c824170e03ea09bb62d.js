3fad24a28f1a4a6e46f31ab0d9c548b32aa3d3c9
Mr.Smoke <kumroshan120@gmail.com>
2023-02-01 00:37:53 +0530

    Updates

---
+++ precache-manifest.13e2e42b62447c824170e03ea09bb62d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97ba2390235f8304dc87",
+    "url": "/CSB_Rajendranagar/static/css/main.cd30ef9c.chunk.css"
+  },
+  {
+    "revision": "97ba2390235f8304dc87",
+    "url": "/CSB_Rajendranagar/static/js/main.8833a41b.chunk.js"
+  },
+  {
+    "revision": "a2788f2d09fdfdef7ca3",
+    "url": "/CSB_Rajendranagar/static/js/runtime~main.2c117d1a.js"
+  },
+  {
+    "revision": "d9ee1702425104a3889a",
+    "url": "/CSB_Rajendranagar/static/js/2.896bae81.chunk.js"
+  },
+  {
+    "revision": "6ba6d76f3556bcac693793bd4e339168",
+    "url": "/CSB_Rajendranagar/index.html"
+  }
+];
