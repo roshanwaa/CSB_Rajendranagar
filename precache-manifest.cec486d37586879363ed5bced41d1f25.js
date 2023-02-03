1d538bbf33749b858488c845d3311e7cbcd1b097
Mr.Smoke <kumroshan120@gmail.com>
2023-02-03 17:25:02 +0530

    Updates

--- precache-manifest.cec486d37586879363ed5bced41d1f25.js
+++ precache-manifest.cec486d37586879363ed5bced41d1f25.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "f612d42a2adceecdc4e8",
+    "revision": "03167714181b4e3dcb87",
     "url": "/CSB_Rajendranagar/static/css/main.ee201fa5.chunk.css"
   },
   {
-    "revision": "f612d42a2adceecdc4e8",
-    "url": "/CSB_Rajendranagar/static/js/main.8533913c.chunk.js"
+    "revision": "03167714181b4e3dcb87",
+    "url": "/CSB_Rajendranagar/static/js/main.1d9f27b9.chunk.js"
   },
   {
     "revision": "a2788f2d09fdfdef7ca3",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/CSB_Rajendranagar/static/js/2.896bae81.chunk.js"
   },
   {
-    "revision": "66bc5329a58df89e0e32af14e4a23af7",
+    "revision": "cdd58b559e5d554343f19fa7a266e8f4",
     "url": "/CSB_Rajendranagar/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/CSB_Rajendranagar/precache-manifest.c22687f15f22d84c6d8e676911824e3f.js"
+  "/CSB_Rajendranagar/precache-manifest.cec486d37586879363ed5bced41d1f25.js"
 );
 
 workbox.clientsClaim();
